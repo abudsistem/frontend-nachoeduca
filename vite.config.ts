61ac81c3cc47a61119f290241b7ae9951606a61c
abudsistem <abudsistem@gmail.com>
2025-03-17 16:17:22 -0500

    third comand

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
+      host: '0.0.0.0',
       '/api': {
         target: 'http://localhost:3001',
         changeOrigin: true,
